90fe4f77a6e5484668fc75a708f834efcf5de719
Leberus <osalvador.vilardaga@gmail.com>
2015-01-28 08:25:23 +0100

    notify.h

---
+++ notify.h
@@ -0,0 +1,17 @@
+#ifndef NOTIFY_H
+	#define NOTIFY_H
+
+#include <sys/inotify.h>
+
+struct __q_events {
+	int event_handled;
+	struct inotify_event i_event;
+};
+
+typedef struct __q_events q_events_t;
+
+#define W_FLAGS	(	IN_CREATE|IN_OPEN| \
+			IN_MODIFY|IN_CLOSE_WRITE| \
+			IN_DELETE|IN_DELETE_SELF )
+
+#endif
